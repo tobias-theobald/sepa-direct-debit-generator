1d2e4ea3a44fe8d39db05012e95456fd02e1dc32
Tobias Theobald <git.diagram859@passmail.net>
2025-03-17 12:33:53 +0100

    Fix off-by-one in stepper

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -15,14 +15,14 @@ const Stepper: React.FC<StepperProps> = ({ steps, currentStep }) => {
             <div className="relative flex flex-col items-center">
               <div
                 className={`w-10 h-10 rounded-full flex items-center justify-center border-2 ${
-                  index < currentStep
+                  index < currentStep - 1
                     ? 'bg-blue-600 border-blue-600 text-white'
-                    : index === currentStep
+                    : index === currentStep - 1
                     ? 'border-blue-600 text-blue-600'
                     : 'border-gray-300 text-gray-300'
                 }`}
               >
-                {index < currentStep ? (
+                {index < currentStep - 1 ? (
                   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                   </svg>
@@ -37,7 +37,7 @@ const Stepper: React.FC<StepperProps> = ({ steps, currentStep }) => {
             {index < steps.length - 1 && (
               <div
                 className={`flex-auto border-t-2 ${
-                  index < currentStep ? 'border-blue-600' : 'border-gray-300'
+                  index < currentStep - 1 ? 'border-blue-600' : 'border-gray-300'
                 }`}
               ></div>
             )}
